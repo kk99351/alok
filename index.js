d1e7b4a067fac5f3c990f4669d980ceef45fae4e
harika <harikahn119@gmail.com>
2023-10-03 17:04:00 +0530

    cors1

--- index.js
+++ index.js
@@ -3,19 +3,7 @@ const app = express();
 
 const cors = require('cors');
 
-const allowedOrigins = ['http://localhost:3000', '*'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
 
 require("dotenv").config();
 const bodyParser = require("body-parser");
@@ -23,6 +11,9 @@ const PORT = process.env.PORT || 8006;
 const pdfMailer = require("./routes/PdfMailer");
 const connectDB = require("./config/ConnectDB");
 
+app.use(cors({
+  origin:'*'
+}));
 
 app.use(bodyParser.json());
 app.use("/", pdfMailer);
