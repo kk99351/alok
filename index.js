8ed43fcdd641bc9493fd53bc8e9ec4b015e5cc81
harika <harikahn119@gmail.com>
2023-10-03 11:48:21 +0530

    cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:3000",
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
   allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
 }));
