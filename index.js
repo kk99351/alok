c8f83ee354815c3bc9a3e62affe70e61dc48f798
harika <harikahn119@gmail.com>
2023-10-03 09:48:17 +0530

    corserror

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const app = express();
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8006;
+
+const corsOptions = {
+  origin: process.env.REACT_APP_DOMAIN_NAME,
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 const cors = require("cors");
-app.use(cors());
+app.use(cors(corsOptions));
 
 const pdfMailer = require("./routes/PdfMailer");
 const connectDB = require("./config/ConnectDB");
 
 app.use(bodyParser.json());
 app.use("/pdf-mailer", pdfMailer);
+
+
 
 app.listen(PORT, async () => {
   try {
