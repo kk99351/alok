dda5a53473e143c858f859821443928b443497b7
alok-56 <alokkumar11746@gmail.com>
2023-10-05 02:15:31 +0530

    changed

--- index.js
+++ index.js
@@ -21,66 +21,56 @@ app.post("/pdf/pdf_veiw", async (req, res) => {
     const { name, phone, citizen, srcCountry, dstCountry, email, Type } =
       req.body;
 
-    console.log(
-      "Request received:",
-      name,
-      phone,
-      citizen,
-      srcCountry,
-      dstCountry,
-      email
-    );
-
     // Generate PDF
-    const htmlContent = PdfTemplate(citizen, dstCountry, Type);
-    const pdfOptions = {
-      format: "Letter",
-      margin: {
-        top: "10mm",
-        right: "10mm",
-        bottom: "10mm",
-        left: "10mm",
-      },
-    };
+    // const htmlContent = PdfTemplate(citizen, dstCountry, Type);
+    // const pdfOptions = {
+    //   format: "Letter",
+    //   margin: {
+    //     top: "10mm",
+    //     right: "10mm",
+    //     bottom: "10mm",
+    //     left: "10mm",
+    //   },
+    // };
 
-    const pdfPath = "generated.pdf"; // Path to save the generated PDF
-    await new Promise((resolve, reject) => {
-      pdf.create(htmlContent, pdfOptions).toFile(pdfPath, (err) => {
-        if (err) {
-          console.error("PDF generation error:", err);
-          return reject(err);
-        }
-        resolve();
-      });
-    });
+    // const pdfPath = "generated.pdf"; // Path to save the generated PDF
+    // await new Promise((resolve, reject) => {
+    //   pdf.create(htmlContent, pdfOptions).toFile(pdfPath, (err) => {
+    //     if (err) {
+    //       console.error("PDF generation error:", err);
+    //       return reject(err);
+    //     }
+    //     resolve();
+    //   });
+    // });
 
-    // Read PDF file
-    const pdfBytes = fs.readFileSync(pdfPath);
+    // // Read PDF file
+    // const pdfBytes = fs.readFileSync(pdfPath);
 
-    // Send email
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      requireTLS: true,
-      auth: {
-        user: "eclecticatmsl23@gmail.com",
-        pass: "okotejdvjinfjwff",
-      },
-      debug: true,
-    });
+    // // Send email
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp.gmail.com",
+    //   port: 587,
+    //   secure: false,
+    //   requireTLS: true,
+    //   auth: {
+    //     user: "eclecticatmsl23@gmail.com",
+    //     pass: "okotejdvjinfjwff",
+    //   },
+    //   debug: true,
+    // });
 
     const mailOptions = {
       from: "eclecticatmsl23@gmail.com",
       to: email,
       subject: "Thank You for Submitting Your Visa Application Form",
       text: `Dear ${name},\n\n...`, // Your email content here
-      attachments: [
-        {
-          filename: "generated.pdf",
-          content: pdfBytes,
-        },
-      ],
+      // attachments: [
+      //   {
+      //     filename: "generated.pdf",
+      //     content: pdfBytes,
+      //   },
+      // ],
     };
 
     await transporter.sendMail(mailOptions);
