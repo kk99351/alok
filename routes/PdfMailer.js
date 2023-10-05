9e790d1858e117ead4a9682384f7284ecbf007cb
alok-56 <alokkumar11746@gmail.com>
2023-10-05 15:34:02 +0530

    alok

--- routes/PdfMailer.js
+++ routes/PdfMailer.js
@@ -8,35 +8,44 @@ const FormPdfmodel = require("../models/FormPdfmodel");
 
 PdfMailer.post("/pdf-mailer", async (req, res) => {
   try {
-    const { name, phone, citizen, srcCountry, dstCountry, email, Type } = req.body;
+    const { name, phone, citizen, srcCountry, dstCountry, email, Type } =
+      req.body;
 
-    console.log("Request received:", name, phone, citizen, srcCountry, dstCountry, email);
+    // console.log(
+    //   "Request received:",
+    //   name,
+    //   phone,
+    //   citizen,
+    //   srcCountry,
+    //   dstCountry,
+    //   email
+    // );
 
-    // Generate PDF
-    const htmlContent = PdfTemplate(citizen, dstCountry, Type);
-    const pdfOptions = {
-      format: "Letter",
-      margin: {
-        top: "10mm",
-        right: "10mm",
-        bottom: "10mm",
-        left: "10mm",
-      },
-    };
+    // // Generate PDF
+    // const htmlContent = PdfTemplate(citizen, dstCountry, Type);
+    // const pdfOptions = {
+    //   format: "Letter",
+    //   margin: {
+    //     top: "10mm",
+    //     right: "10mm",
+    //     bottom: "10mm",
+    //     left: "10mm",
+    //   },
+    // };
 
-    const pdfPath = "generated.pdf"; // Path to save the generated PDF
-    await new Promise((resolve, reject) => {
-      pdf.create(htmlContent, pdfOptions).toFile(pdfPath, (err) => {
-        if (err) {
-          console.error("PDF generation error:", err);
-          return reject(err);
-        }
-        resolve();
-      });
-    });
+    // const pdfPath = "generated.pdf"; // Path to save the generated PDF
+    // await new Promise((resolve, reject) => {
+    //   pdf.create(htmlContent, pdfOptions).toFile(pdfPath, (err) => {
+    //     if (err) {
+    //       console.error("PDF generation error:", err);
+    //       return reject(err);
+    //     }
+    //     resolve();
+    //   });
+    // });
 
-    // Read PDF file
-    const pdfBytes = fs.readFileSync(pdfPath);
+    // // Read PDF file
+    // const pdfBytes = fs.readFileSync(pdfPath);
 
     // Send email
     const transporter = nodemailer.createTransport({
@@ -53,15 +62,15 @@ PdfMailer.post("/pdf-mailer", async (req, res) => {
 
     const mailOptions = {
       from: "eclecticatmsl23@gmail.com",
-      to: email,
+      to: "alokkumar11746@gmail.com",
       subject: "Thank You for Submitting Your Visa Application Form",
-      text: `Dear ${name},\n\n...`, // Your email content here
-      attachments: [
-        {
-          filename: "generated.pdf",
-          content: pdfBytes,
-        },
-      ],
+      text: `Dear`, // Your email content here
+      // attachments: [
+      //   {
+      //     filename: "generated.pdf",
+      //     content: pdfBytes,
+      //   },
+      // ],
     };
 
     await transporter.sendMail(mailOptions);
