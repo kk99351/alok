2a9005970e4f309aaf505840dcd28e864f94207b
alok-56 <alokkumar11746@gmail.com>
2023-10-06 15:35:36 +0530

    alok

--- routes/PdfMailer.js
+++ routes/PdfMailer.js
@@ -21,81 +21,73 @@ PdfMailer.post("/pdf-mailer", async (req, res) => {
       email
     );
 
-    // Generate PDF
-    // const htmlContent = PdfTemplate(citizen, dstCountry, Type);
-    // const pdfOptions = {
-    //   format: "Letter",
-    //   margin: {
-    //     top: "10mm",
-    //     right: "10mm",
-    //     bottom: "10mm",
-    //     left: "10mm",
-    //   },
-    // };
+   // Generate PDF
+    const htmlContent = PdfTemplate(citizen, dstCountry, Type);
+    const pdfOptions = {
+      format: "Letter",
+      margin: {
+        top: "10mm",
+        right: "10mm",
+        bottom: "10mm",
+        left: "10mm",
+      },
+    };
 
-    // const pdfBytes = await new Promise((resolve, reject) => {
-    //   html_to_pdf.generatePdf(
-    //     { content: htmlContent },
-    //     pdfOptions,
-    //     (error, buffer) => {
-    //       if (error) return reject(error);
+    const pdfBytes = await new Promise((resolve, reject) => {
+      html_to_pdf.generatePdf(
+        { content: htmlContent },
+        pdfOptions,
+        (error, buffer) => {
+          if (error) return reject(error);
 
-    //       resolve(buffer);
-    //     }
-    //   );
-    // });
-
-    fs.readFile("generated.pdf", async (err, pdfBuffer) => {
-      if (pdfBuffer) {
-        const transporter = nodemailer.createTransport({
-          host: "smtp.gmail.com",
-          port: 587,
-          secure: false,
-          requireTLS: true,
-          auth: {
-            user: "eclecticatmsl23@gmail.com",
-            pass: "okotejdvjinfjwff",
-          },
-          debug: true,
-        });
-
-        const mailOptions = {
-          from: "eclecticatmsl23@gmail.com",
-          to: email,
-          subject: "Thank You for Submitting Your Visa Application Form",
-          text: `Dear`, // Your email content here
-          attachments: [
-            {
-              filename: "generated.pdf",
-              content: pdfBuffer,
-            },
-          ],
-        };
-
-
-       
-
-        await transporter.sendMail(mailOptions);
-        res.send("application")
-        // const newUser = await FormPdfmodel.create({
-        //   name,
-        //   email,
-        //   phone,
-        //   citizen,
-        //   srcCountry,
-        //   dstCountry,
-        // });
-
-        // // Respond with success message
-        // res
-        //   .status(200)
-        //   .json({ message: "Email sent successfully", user: newUser });
-      }
+          resolve(buffer);
+        }
+      );
     });
 
-    // Send email
+   
 
-    // Save user data to the database
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      auth: {
+        user: "eclecticatmsl23@gmail.com",
+        pass: "okotejdvjinfjwff",
+      },
+      debug: true,
+    });
+
+    const mailOptions = {
+      from: "eclecticatmsl23@gmail.com",
+      to: email,
+      subject: "Thank You for Submitting Your Visa Application Form",
+      text: `Dear`, // Your email content here
+      attachments: [
+        {
+          filename: "generated.pdf",
+          content: pdfBytes,
+        },
+      ],
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    const newUser = await FormPdfmodel.create({
+          name,
+          email,
+          phone,
+          citizen,
+          srcCountry,
+          dstCountry,
+        });
+
+        // Respond with success message
+        res
+          .status(200)
+          .json({ message: "Email sent successfully", user: newUser })
+
   } catch (error) {
     console.error("Server error:", error);
     res.status(500).json({ error: "Internal server error" });
